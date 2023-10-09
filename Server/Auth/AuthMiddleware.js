dbd468d9949c1c3c4537e40ec7185cf2ddee2d2e
davidkenesaw <davidkennesaw@gmail.com>
2023-10-09 02:29:33 -0400

    deleted logs

--- Server/Auth/AuthMiddleware.js
+++ Server/Auth/AuthMiddleware.js
@@ -1,10 +1,7 @@
 
 function verifyAccountType(req,res,next){
-    //verify if token is on users browser
-    console.log(req.cookies.User.AccountType)
+    //verify if user is owner or admin
     if(req.cookies.User.AccountType != "Owner" && req.cookies.User.AccountType != "Admin"){
-        console.log(req.cookies.User.AccountType)
-        console.log("it doesnt")
         return res.redirect("/")
     }next()
 }
