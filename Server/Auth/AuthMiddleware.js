c0dd8f53eebd4765cc843e4ebfa82ff7dc2f90b9
davidkenesaw <77091094+davidkenesaw@users.noreply.github.com>
2023-10-24 14:54:43 -0400

    token bug fix

--- Server/Auth/AuthMiddleware.js
+++ Server/Auth/AuthMiddleware.js
@@ -9,8 +9,11 @@ function verifyAccountType(req,res,next){
 
 async function verifyToken(req,res,next){
     //verify if token is on users browser
+    if(!req.cookies.Token){
+        return res.redirect("/Login")
+    }
     let response = await DecodeToken(req.cookies.Token)
-    if(response.error == "Invalid token" || response.error == "expired" || !req.cookies.Token){
+    if(response.error == "Invalid token" || response.error == "expired"){
         return res.redirect("/Login")
     }next()
 }
